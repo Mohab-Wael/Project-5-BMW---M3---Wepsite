45f614eabf8bfa56a25bf270db1f86699bb35764
Mohab-Wael <154028855+Mohab-Wael@users.noreply.github.com>
2025-10-28 00:56:18 +0300

    Add files via upload

---
+++ BmwProject/index.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+
+class clsPerson
+{
+  private :
+    string FirstName;
+
+
+
+}
+
+
+
+
+int main  ()
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+
+
+
+
+
+
+
+
+
+    return 0;
+}
